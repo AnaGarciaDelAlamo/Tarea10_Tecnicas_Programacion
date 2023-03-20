3f6332bc159c5c7c93f504beef7243a072b763b1
ana <andelamo@myuax.com>
2023-03-20 17:34:29 +0100

    metodo suma

--- src/main/java/Ejercicio2/Operacion.java
+++ src/main/java/Ejercicio2/Operacion.java
@@ -1,4 +1,13 @@
 package Ejercicio2;
 
 public class Operacion {
+
+    public int suma(int a, int b) throws DesbordaCapacidadException{
+        long resultado = (long)a + (long)b;
+        if (resultado > Integer.MAX_VALUE || resultado < Integer.MIN_VALUE){
+            throw new DesbordaCapacidadException("El resultado de la suma es demasiado grande");
+        } else {
+            return (int)resultado;
+        }
+    }
 }
